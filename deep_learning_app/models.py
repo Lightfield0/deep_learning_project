0fae44eda77877e9ff05e24d355c65bb38b67e38
SKYWALKERT1 <furkan-fatih-ciftci@hotmail.com>
2023-11-16 23:22:28 +0300

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is behind 'origin/main' by 1 commit, and can be fast-forwarded.
       (use "git pull" to update your local branch)
    
     Changes not to be committed:
    	modified:   deep_learning_app/__pycache__/__init__.cpython-311.pyc
    	modified:   deep_learning_app/__pycache__/admin.cpython-311.pyc
    	modified:   deep_learning_app/__pycache__/apps.cpython-311.pyc
    	modified:   deep_learning_app/__pycache__/models.cpython-311.pyc
    	modified:   deep_learning_app/__pycache__/urls.cpython-311.pyc
    	modified:   deep_learning_app/__pycache__/views.cpython-311.pyc
    	modified:   deep_learning_app/migrations/__pycache__/__init__.cpython-311.pyc
    	modified:   deep_learning_app/models.py
    	modified:   deep_learning_app/templates/about_us.html
    	modified:   deep_learning_app/urls.py
    	modified:   deep_learning_project/__pycache__/__init__.cpython-311.pyc
    	modified:   deep_learning_project/__pycache__/settings.cpython-311.pyc
    	modified:   deep_learning_project/__pycache__/urls.cpython-311.pyc
    	modified:   deep_learning_project/__pycache__/wsgi.cpython-311.pyc
    	modified:   deep_learning_project/settings.py
    	new file:   icon/FusionFlowNet.jpg

--- deep_learning_app/models.py
+++ deep_learning_app/models.py
@@ -1,3 +1,6 @@
 from django.db import models
+class YourModel(models.Model):
+    # Diğer alanlar
+    image = models.ImageField(upload_to='FusionFlowNet.jpg/')
 
 # Create your models here.
--- deep_learning_app/urls.py
+++ deep_learning_app/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.home, name="home"),
@@ -8,3 +9,5 @@ urlpatterns = [
     path('types-of-nn/', views.types_of_nn, name="types-of-nn"),
     path('about-us/', views.about_us, name="about-us")
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
